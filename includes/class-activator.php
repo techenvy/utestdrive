71bc39c319e8eec2f56e5a16a901fdfa66fe71f7
Rao Abid <rao@booskills.com>
2020-01-11 15:58:44 +0500

    Multisite check on installation, activate for only main site

--- includes/class-activator.php
+++ includes/class-activator.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Utestdrive;
 /**
  * Fired during plugin activation
@@ -31,6 +32,18 @@ class Activator {
 	 */
 	public static function activate() {
 
+		// Activate only if its a multi site setup and main site of network
+		if ( ! is_multisite() || ! is_main_site( get_current_blog_id() ) ) {
+			deactivate_plugins( UTESTDRIVE_PLUGIN_BASE_NAME );
+			wp_die(
+				'<p>' .
+				sprintf(
+					'uTestDrive plugin requires %s, it can not do anything for a non-network installation',
+					'<a href="https://wordpress.org/support/article/create-a-network/" target="_blank">' . esc_html__( 'WordPress Site Network', 'utestdrive' ) . '</a>'
+				)
+				. '</p> <a href="' . admin_url( 'plugins.php' ) . '">' . esc_html__( 'go back', '' ) . '</a>'
+			);
+		}
 
 	}
 
